a284448d128d783dce15b999b11f9bf05ef09659
brhcareerinfo-collab <brhcareer.info@gmail.com>
2026-07-20 04:03:02 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,30 @@
+import { db } from "./firebase.js";
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
+const table = document.getElementById("memberTable");
+
+async function loadMembers() {
+
+  const querySnapshot = await getDocs(collection(db, "members"));
+
+  querySnapshot.forEach((doc) => {
+
+    const data = doc.data();
+
+    table.innerHTML += `
+      <tr>
+        <td>${data.fullName || ""}</td>
+        <td>${data.mobile || ""}</td>
+        <td>${data.post || ""}</td>
+        <td>₹${data.fee || ""}</td>
+        <td>${data.status || "Pending"}</td>
+      </tr>
+    `;
+  });
+
+}
+
+loadMembers();
