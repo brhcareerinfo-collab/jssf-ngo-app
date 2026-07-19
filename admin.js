9afc66ded79be2d327579e8f40f83f0e84eb2f6a
brhcareerinfo-collab <brhcareer.info@gmail.com>
2026-07-20 04:32:15 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,7 +1,9 @@
 import { db } from "./firebase.js";
 import {
   collection,
-  getDocs
+  getDocs,
+  doc,
+  updateDoc
 } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
 const table = document.getElementById("memberTable");
@@ -24,13 +26,17 @@ async function loadMembers() {
         <td>₹${data.fee || ""}</td>
         <td>${data.status || "Pending"}</td>
         <td>
-          <button class="btn btn-success btn-sm">
-            Approve
-          </button>
+          <button
+class="btn btn-success btn-sm"
+onclick="approveMember('${doc.id}')">
+Approve
+</button>
 
-          <button class="btn btn-danger btn-sm">
-            Reject
-          </button>
+<button
+class="btn btn-danger btn-sm"
+onclick="rejectMember('${doc.id}')">
+Reject
+</button>
         </td>
       </tr>
     `;
@@ -40,3 +46,26 @@ async function loadMembers() {
 }
 
 loadMembers();
+window.approveMember = async function(id) {
+
+  await updateDoc(doc(db, "members", id), {
+    status: "Approved"
+  });
+
+  alert("Member Approved");
+
+  loadMembers();
+
+}
+
+window.rejectMember = async function(id) {
+
+  await updateDoc(doc(db, "members", id), {
+    status: "Rejected"
+  });
+
+  alert("Member Rejected");
+
+  loadMembers();
+
+}
