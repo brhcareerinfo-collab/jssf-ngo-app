1f4af6eb5da0a01a81ae5662db74d46dca4935e4
brhcareerinfo-collab <brhcareer.info@gmail.com>
2026-07-20 02:20:43 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,26 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 
+const firebaseConfig = {
+
+  apiKey: "YOUR_API_KEY",
+
+  authDomain: "YOUR_AUTH_DOMAIN",
+
+  projectId: "jssf-member-portal",
+
+  storageBucket: "YOUR_STORAGE_BUCKET",
+
+  messagingSenderId: "1072736110249",
+
+  appId: "YOUR_APP_ID"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export const auth = getAuth(app);
