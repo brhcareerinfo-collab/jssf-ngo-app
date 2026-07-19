59e66a1b87a9c86b4799eae26b62b56d4c8d6522
brhcareerinfo-collab <brhcareer.info@gmail.com>
2026-07-20 04:20:27 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -8,6 +8,8 @@ const table = document.getElementById("memberTable");
 
 async function loadMembers() {
 
+  table.innerHTML = "";
+
   const querySnapshot = await getDocs(collection(db, "members"));
 
   querySnapshot.forEach((doc) => {
@@ -15,25 +17,24 @@ async function loadMembers() {
     const data = doc.data();
 
     table.innerHTML += `
-      table.innerHTML += `
-<tr>
-  <td>${data.fullName || ""}</td>
-  <td>${data.mobile || ""}</td>
-  <td>${data.post || ""}</td>
-  <td>₹${data.fee || ""}</td>
-  <td>${data.status || "Pending"}</td>
-  <td>
-    <button class="btn btn-success btn-sm">
-      Approve
-    </button>
+      <tr>
+        <td>${data.fullName || ""}</td>
+        <td>${data.mobile || ""}</td>
+        <td>${data.post || ""}</td>
+        <td>₹${data.fee || ""}</td>
+        <td>${data.status || "Pending"}</td>
+        <td>
+          <button class="btn btn-success btn-sm">
+            Approve
+          </button>
 
-    <button class="btn btn-danger btn-sm mt-1">
-      Reject
-    </button>
-  </td>
-</tr>
-`;
+          <button class="btn btn-danger btn-sm">
+            Reject
+          </button>
+        </td>
+      </tr>
     `;
+
   });
 
 }
