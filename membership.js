0bdb4d916176926f0be4e8b93407453e179e4547
brhcareerinfo-collab <brhcareer.info@gmail.com>
2026-07-20 02:39:17 +0530

    Update membership.js

--- membership.js
+++ membership.js
@@ -1,1 +1,16 @@
+import { db } from "./firebase.js";
+import {
+  collection,
+  addDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
+const form = document.getElementById("memberForm");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  alert("Connection Successful");
+
+  // अगले स्टेप में पूरा Firestore Save कोड जोड़ेंगे
+});
